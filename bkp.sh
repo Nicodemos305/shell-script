bf69d774ba88b4e00904bc5ad973da4505b46458
Felipe Nicodemos <felipe_nicodemos@hotmail.com>
2019-10-25 17:58:00 -0300

    Update bkp.sh

--- bkp.sh
+++ bkp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 while true
 do
-	data=$(date "+%Y-%m-%d-%H-%M.%S")
-	cp $PATH_BKP ./$data
-	echo $data
+  data=$(date "+%Y-%m-%d-%H-%M.%S")
+  cp $PATH_BKP ./$data
+  $data
 sleep $TIME_BKP
 done
