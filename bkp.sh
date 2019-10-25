1a43bd12a36562357918d4576fc5448a60afb9cd
Felipe Oliveira <felipe_nicodemos@hotmail.com>
2019-10-25 18:17:23 -0300

    Add tar.gz bkp

--- bkp.sh
+++ bkp.sh
@@ -2,7 +2,7 @@
 while true
 do
 	data=$(date "+%Y-%m-%d-%H-%M.%S")
-	cp $PATH_BKP ./$data
-	echo $data
+	tar -czvf ./bkp-$data.tar.gz $PATH_BKP
+	echo "Backup done! $PATH_BKP $data"
 sleep $TIME_BKP
 done
